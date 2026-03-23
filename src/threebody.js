5d0ad3dc5448c5b2ceb72df5a1f12e175121680b
JialaChang <jiala20061218@gmail.com>
2026-03-23 23:06:23 +0800

    優化三體特效與背景

--- src/threebody.js
+++ src/threebody.js
@@ -1,6 +1,10 @@
 // ====== 1. 引用模組與基本設定 ======
 import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+// 後期處理模組
+import {EffectComposer} from 'three/addons/postprocessing/EffectComposer.js';
+import {RenderPass} from 'three/addons/postprocessing/RenderPass.js';
+import {UnrealBloomPass} from 'three/addons/postprocessing/UnrealBloomPass.js';
 
 // 建立場景物件
 const scene = new THREE.Scene();
@@ -44,10 +48,50 @@ const star3Pos = [0.5, 0, -2];
 const star3Vel = [0.2, 0, 0.1];
 
 // 最大軌跡數
-const pointsMax = 5000;
+const pointsMax = 2000;
 
 
-// ====== 3. 星體類別 ======
+// ====== 3. 添加背景星星 ======
+const maxStarNum = 5000;  // 星星數量
+const createBackgroundStars = () => {
+    // 儲存星星座標
+    const array = [];
+    for (let i = 0; i < maxStarNum; ++i) {
+        let x, y, z;
+        do {
+            x = (Math.random() - 0.5) * 2000;
+            y = (Math.random() - 0.5) * 2000;
+            z = (Math.random() - 0.5) * 2000;
+        } while (
+            Math.abs(x) < 100 && Math.abs(y) < 100 && Math.abs(x) < 100
+        );
+        array.push(x, y, z);
+    }
+
+    const geometry = new THREE.BufferGeometry();
+    // GPU 加速
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(array, 3));
+
+    // 使用圓形貼圖
+    const loader = new THREE.TextureLoader();
+    const starTexture = loader.load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/sprites/disc.png');
+
+    // 渲染星星
+    const material = new THREE.PointsMaterial({
+        color: 0xffffff,        // 白色
+        size: 1.5,
+        map: starTexture,
+        sizeAttenuation: true,  // 尺寸衰減
+        transparent: true,
+        alphaTest: 0.5,         // 設定渲染門檻
+    });
+    const backgroundStars = new THREE.Points(geometry, material);
+    scene.add(backgroundStars);
+};
+createBackgroundStars();
+
+
+// ====== 4. 星體建構及更新加速度 ======
 class Star {
 
     // 建構子
@@ -64,13 +108,16 @@ class Star {
         // 球體骨架 (半徑, 水平分段數, 垂直分段數)
         const starGeo = new THREE.SphereGeometry(0.05, 32, 32);
         // 球體皮膚
-        const starMat = new THREE.MeshPhongMaterial({color: color, emissive: color, emissiveIntensity: 5});
+        const starMat = new THREE.MeshStandardMaterial({
+            color: color,
+            emissive: color,
+            emissiveIntensity: 2.5,
+            transparent: true,
+            alphaTest: 0.9,
+        });
         // 融合骨架和皮膚並設定位置
         this.mesh = new THREE.Mesh(starGeo, starMat);
         this.mesh.position.copy(this.currPos);
-        // 建立發光物件跟隨球體
-        this.light = new THREE.PointLight(color);
-        this.mesh.add(this.light);
         // 將球體放入場景
         scene.add(this.mesh);
         
@@ -81,10 +128,12 @@ class Star {
         const lineMat = new THREE.LineBasicMaterial({
             color: color,
             transparent: true,
-            opacity: 0.5
+            opacity: 0.8
         });
         this.line = new THREE.Line(this.lineGeo, lineMat);
         scene.add(this.line);
+        // 永久渲染軌跡線
+        this.line.frustumCulled = false;
     }
 
     // 更新速度與位置
@@ -111,17 +160,31 @@ class Star {
 }
 
 
-// ====== 4. 初始化星體 ======
+// ====== 5. 初始化星體 ======
 const stars = [
-    new Star(0xff4444, star1Mass, star1Pos, star1Vel),  // 紅星
-    new Star(0x44ff44, star2Mass, star2Pos, star2Vel),  // 綠星
-    new Star(0x4444ff, star3Mass, star3Pos, star3Vel)   // 藍星
+    new Star(0xff6600, star1Mass, star1Pos, star1Vel),  // 橘星
+    new Star(0x66ccff, star2Mass, star2Pos, star2Vel),  // 藍星
+    new Star(0xfff0b3, star3Mass, star3Pos, star3Vel)   // 白星
 ];
 
 
-// ====== 5. 動畫與物理計算的迴圈 ======
+// ====== 6. 設置後期處理 ======
+const renderScene = new RenderPass(scene, camera);
+const bloomPass = new UnrealBloomPass(
+    new THREE.Vector2(window.innerWidth, window.innerHeight),
+    1.2,  // 強度
+    1.2,  // 半徑
+    0.1   // 閾值
+);
+
+const composer = new EffectComposer(renderer);
+composer.addPass(renderScene);
+composer.addPass(bloomPass);
+
+
+// ====== 7. 動畫與物理計算的迴圈 ======
 const vector = new THREE.Vector3();  // 計算位移矢量
-const accG = new THREE.Vector3();  // 計算加速度
+const accG = new THREE.Vector3();    // 計算加速度
 
 function animate() {
     // 在下一次繪製螢幕時會執行這個函式 => 無限迴圈
@@ -172,7 +235,7 @@ function animate() {
     // 讓滑鼠能拖曳視角
     controls.update();
     // 執行渲染
-    renderer.render(scene, camera);
+    composer.render();
 }
 
 // 執行質心修正
@@ -194,10 +257,13 @@ stars.forEach(s => s.points = []);
 animate();
 
 
-// --- 6. 處理視窗縮放 ---
+// --- 8. 處理視窗縮放 ---
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    // 更新後期處理的分辨率
+    composer.setSize(window.innerWidth, window.innerHeight);
+    bloomPass.resolution.set(window.innerWidth, window.innerHeight);
 });
 
